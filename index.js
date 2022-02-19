5cc53f2de93a9d0b7feb92b3efb9b8352814aebb
kaijin1 <2245963087@qq.com>
2022-02-19 23:15:23 +0800

    安装中间件
    jsonwebtoken--jwt
    koa-body--post请求和文件上传
    koa-json-error--错误处理
    koa-parameter--参数校验
    koa-router--路由
    mongoose--mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const Koa = require('Koa')
+const Koa = require('Koa');
+const Router = require('koa-router');
 const app = new Koa()
+const router = new Router()
+
+app.use(router.routes())
 
 app.listen(3000,()=>{
   console.log('koa 监听 3000 端口');
