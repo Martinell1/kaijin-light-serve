a5c27cb23753f3730f150b89c1911825647fecbe
AsheOne <1029129827@qq.com>
2022-03-11 14:19:53 +0800

    实现用户头像上传

---
+++ app/config/qiniu.js
@@ -0,0 +1,9 @@
+const accessKey = 'I8vWDivyRnXM85nLUMuaTr3HN97lX_B4bGugbpGV';
+const secretKey = 'IzHCng7bUdhScYecf25vuaUtynjreSAlJ7PLfg2O';
+const bucket = 'light-hub'
+
+module.exports = {
+  accessKey,
+  secretKey,
+  bucket
+}
--- app/route/home.js
+++ app/route/home.js
@@ -1,7 +1,27 @@
-const Router = require("koa-router");
-const router = new Router()
+const {SECRET} = require('../config/constance')
+const jwt = require('koa-jwt')
+const auth = jwt({secret:SECRET})
+const {accessKey,secretKey,bucket} = require("../config/qiniu")
+const qiniu = require('qiniu')
 
-router.get('/',(ctx)=>{
-  ctx.body = 'home'
-})
+const Router = require("koa-router");
+const router = new Router({prefix:'/home'})
+
+const uploadImage = async ctx =>{
+  const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+  const options = {
+    scope: bucket,
+    expires: 7200
+  };
+  const putPolicy = new qiniu.rs.PutPolicy(options);
+  const uploadToken = putPolicy.uploadToken(mac);
+  if(uploadToken){
+    console.log('token',uploadToken);
+    ctx.body = uploadToken
+  }
+}
+
+
+router.post('/uploadImage',auth,uploadImage) //
 module.exports = router
