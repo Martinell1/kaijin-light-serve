725f279168fac68ad900b3c1fba79a4e503eca3e
AsheOne <1029129827@qq.com>
2022-04-09 13:39:06 +0800

    s

--- app/model/topic.js
+++ app/model/topic.js
@@ -6,7 +6,7 @@ const topicSchema = new Schema({
   __v:{type:Number,select:false},
   name:{type:String,required:true},
   avatar_url:{type:String},
-  introduction:{type:String,select:false},
+  introduction:{type:String,select:true},
 },{timestamps:true})
 
 module.exports = model('Topic',topicSchema)
